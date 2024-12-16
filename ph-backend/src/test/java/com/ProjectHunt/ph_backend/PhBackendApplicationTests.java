5cbacbb17d5c304d28bbb28d28102aa578c89257
diyaprajapati <diyabprajapati2005@gmail.com>
2024-12-16 20:33:38 +0530

    initialized backend

---
+++ ph-backend/src/test/java/com/ProjectHunt/ph_backend/PhBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ProjectHunt.ph_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
