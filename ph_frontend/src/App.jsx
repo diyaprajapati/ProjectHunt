e8d7e6c487f1133dde21390032feee6a5dc0068f
diyaprajapati <diyabprajapati2005@gmail.com>
2024-12-18 00:36:36 +0530

    finished landing page

--- ph_frontend/src/App.jsx
+++ ph_frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './index.css'
-import { ChevronLeft, ChevronRight, Code, Sparkle } from 'lucide-react';
+import { ArrowUp, ChevronLeft, ChevronRight, Code, Sparkle } from 'lucide-react';
 
 export default function App() {
   return (
@@ -29,6 +29,33 @@ export default function App() {
       <div className='mt-4 flex w-full justify-center items-center'>
         <input type='search' placeholder='Search projects, tags or creators...' className='border-none rounded-full h-11 w-[90%] px-4 py-5 bg-gray-800 text-zinc-400 md:w-[50%]'/>
         <Sparkle className='absolute right-[8%] text-zinc-500 md:right-[26%]' />
+      </div>
+
+      {/* project names */}
+      <div className='flex items-center justify-center'>
+        <div className='text-white bg-gray-800 mt-14 w-[95%] flex items-center rounded-lg px-4 py-5 md:w-[55%] justify-between'>
+          <div className='flex flex-col gap-1'>
+            <div className='flex gap-3'>
+              <div className='text-lg font-bold'>
+                Project title
+              </div>
+              <div className='flex items-center bg-blue-900 px-2 rounded-full text-xs h-6 text-blue-300 font-medium'>
+                tech
+              </div>
+            </div>
+            <div className='text-gray-400 font-medium'>
+              Description
+            </div>
+            <div className='text-gray-500'>
+              created by
+            </div>
+          </div>
+          {/* upvote */}
+          <div className='bg-gray-700 rounded-full  px-4 py-2 flex cursor-pointer'>
+            <ArrowUp className='size-5 w-5 h-5 mr-2' />
+            <div className='flex items-center font-medium text-base'>0</div>
+          </div>
+        </div>
       </div>
 
     </div>
