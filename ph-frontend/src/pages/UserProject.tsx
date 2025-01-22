2bbfb5a81f0f29a20ba1b91fc3c1a5a471c3bf41
diyaprajapati <diyabprajapati2005@gmail.com>
2025-01-22 08:53:24 +0530

    modify ui

--- ph-frontend/src/pages/UserProject.tsx
+++ ph-frontend/src/pages/UserProject.tsx
@@ -3,6 +3,7 @@ import axios from "axios";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { ArrowUp } from "lucide-react";
 
 interface Project {
   id: number;
@@ -51,8 +52,8 @@ const UserProjects: React.FC = () => {
   }, [projects, searchQuery, sortBy]);
 
   return (
-    <div className="min-h-screen bg-gradient-to-br from-gray-900 to-blue-900 p-5">
-      <div className="flex justify-between items-center mb-6">
+    <div className="min-h-screen bg-gradient-to-br from-gray-900 to-blue-900 md:p-16 p-6">
+      <div className="flex justify-between items-center mb-10">
         <h1 className="text-white font-bold text-4xl">Your Projects</h1>
         <Button className="bg-blue-700 hover:bg-blue-800">+ New Project </Button>
       </div>
@@ -107,24 +108,32 @@ const UserProjects: React.FC = () => {
 
       {/* Projects List */}
       {filteredProjects.length > 0 ? (
-        <div className="flex flex-col gap-3">
+        <div className="flex flex-col gap-3 justify-center self-center items-center">
           {filteredProjects.map((project) => (
             <div
               key={project.id}
-              className="bg-gray-800 px-4 py-5 rounded-lg shadow hover:shadow-lg transition"
+              className="bg-gray-800/50 hover:bg-gray-800/70 px-4 py-5 rounded-lg shadow hover:shadow-lg transition md:w-[90%] w-full flex justify-between items-center"
             >
-              <a
-                href={project.websiteLink}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-blue-400 hover:underline"
-              >
-                <h2 className="text-lg font-bold text-white mb-2 hover:text-blue-400">
-                  {project.name}
-                </h2>
-              </a>
-              <p className="text-gray-400 text-base mb-4 font-semibold">{project.description}</p>
-              <p className="text-gray-300 mt-2">Upvotes: {project.upvotes || 0}</p>
+              <div>
+                <a
+                  href={project.websiteLink}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-blue-400 hover:underline"
+                >
+                  <h2 className="text-lg font-bold text-white mb-2 hover:text-blue-400">
+                    {project.name}
+                  </h2>
+                </a>
+                <p className="text-gray-400 text-base mb-4 font-semibold">{project.description}</p>
+              </div>
+              <div>
+                {/* <p className="text-gray-300 mt-2">Upvotes: {project.upvotes || 0}</p> */}
+                <div className="flex items-center gap-2 bg-gray-700/80 py-3 rounded-full px-4 hover:bg-gray-700/90 transition-all cursor-pointer">
+                  <ArrowUp className="text-white" />
+                  <p className="text-gray-300">{project.upvotes || 0}</p>
+                </div>
+              </div>
             </div>
           ))}
         </div>
