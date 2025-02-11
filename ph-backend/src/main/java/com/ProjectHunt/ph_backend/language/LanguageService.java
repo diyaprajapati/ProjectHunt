2b084b6a7940de29ff6f8089dd74202f7f996c3f
diyaprajapati <diyabprajapati2005@gmail.com>
2025-02-11 12:00:17 +0530

    fix language error

--- ph-backend/src/main/java/com/ProjectHunt/ph_backend/language/LanguageService.java
+++ ph-backend/src/main/java/com/ProjectHunt/ph_backend/language/LanguageService.java
@@ -1,10 +1,12 @@
 package com.ProjectHunt.ph_backend.language;
 
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 //@Service
@@ -26,12 +28,21 @@ public class LanguageService {
     @Autowired
     private LanguageRepository languageRepository;
 
+//    public List<Language> getAllLanguagesById(List<Long> ids) {
+//        if (ids == null || ids.isEmpty()) {
+//            return new ArrayList<>();
+//        }
+//        List<Language> languages = languageRepository.findAllById(ids);
+//        System.out.println("Found languages: " + languages); // Debug log
+//        return languages;
+//    }
     public List<Language> getAllLanguagesById(List<Long> ids) {
         if (ids == null || ids.isEmpty()) {
             return new ArrayList<>();
         }
-        List<Language> languages = languageRepository.findAllById(ids);
+        List<Language> languages = new ArrayList<>(languageRepository.findAllByIds(new HashSet<>(ids)));
         System.out.println("Found languages: " + languages); // Debug log
         return languages;
     }
+
 }
